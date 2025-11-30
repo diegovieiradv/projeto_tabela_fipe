22a920204118b713768b8475e543927159a5ba8a
Diego Vieira <dvdiego87@gmail.com>
2025-11-30 03:13:05 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/projeto_tabela_fipe/ProjetoTabelaFipeApplication.java
@@ -0,0 +1,16 @@
+package br.com.alura.projeto_tabela_fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTabelaFipeApplication.class, args);
+	}
+olaaaa
+
+
+    
+}
